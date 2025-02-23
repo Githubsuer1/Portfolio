daf4f445804196dd55fa48666e8b6a20ffdafb88
Githubsuer1 <mishrasiddhant560@gmail.com>
2025-02-23 11:42:13 +0530

    skills and contact details updated

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -14,36 +14,50 @@ const Contact = () => {
         <div className="flex sm:flex-row flex-col sm:gap-6 gap-8 p-8">
 
           <div className="flex gap-4">
-            <NavLink className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
+            <NavLink 
+            to="https://www.instagram.com/siddhant_chandrashekhar_mishra?igsh=emg4bzFwNW0yMWFi"
+            className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
             transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 hover:bg-pink-500 hover:text-white">
               <FaSquareInstagram size={48}/>
             </NavLink >
-            <NavLink className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
+            <NavLink 
+            to="https://www.facebook.com/siddhant.mishra.106902?mibextid=rS40aB7S9Ucbxw6v"
+            className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
             transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 hover:bg-blue-500 hover:text-white">
               <FaFacebookSquare size={48}/>
             </NavLink>
           </div>
 
           <div className="flex gap-4">
-            <NavLink className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
+            <NavLink 
+            to="https://x.com/sidd_mis?t=okxZLHouGdevsrajTvhtYQ&s=08"
+            className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
             transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 hover:bg-black hover:text-white">
               <FaSquareXTwitter size={48}/>
             </NavLink>
-            <NavLink className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
+            <NavLink
+            to="https://www.linkedin.com/in/siddhant-mishra-developer/" 
+            className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
             transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 hover:bg-blue-700 hover:text-white">
               <FaLinkedin size={48}/>
             </NavLink>
           </div>
 
           <div className="flex gap-4">
-            <NavLink className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
+            <NavLink 
+            to="https://github.com/Githubsuer1"
+            className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
             transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 hover:text-white hover:bg-black">
               <FaGithubSquare size={48}/>
             </NavLink>
-            <NavLink className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
-            transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 hover:bg-red-600 hover:text-white">
-              <MdEmail size={48}/>
-            </NavLink>
+            <a 
+              href="mailto:mishrasiddhant560@gmail.com?subject=Contact from Portfolio&body=Hello Siddhant,"
+              className="p-3 shadow-2xl rounded-full w-24 h-24 flex items-center justify-center
+              transition-all duration-700 ease hover:scale-110 transform hover:-translate-y-4 
+            hover:bg-red-600 hover:text-white"
+            >
+            <MdEmail size={48} />
+            </a>
           </div>
           
 
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -5,10 +5,10 @@ const Skills = () => {
     <section id="skills" className="min-h-screen h-full bg-white flex flex-col items-center p-4">
         <h2 className="text-3xl text-center p-4 font-bold text-gray-800">Skills</h2>
         <div className='w-full max-w-3xl grid sm:grid-cols-2 gap-4 place-items-center'>
-          <FlipCard domen="Front-end" technology="HTML CSS JAVASCRIPT"/>
-          <FlipCard domen="Back-end" technology="Nodejs Expressjs" />  
+          <FlipCard domen="Front-end" technology="HTML, CSS, JAVASCRIPT, REACT"/>
+          <FlipCard domen="Back-end" technology="Nodejs, Expressjs" />  
           <FlipCard domen="Database" technology="MONGODB" />  
-          <FlipCard domen="Version-control" technology="GIT GITHUB" />  
+          <FlipCard domen="Version-control" technology="GIT, GITHUB" />  
         </div>
     </section>
   );
